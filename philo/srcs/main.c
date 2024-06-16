fac93a7792bdd25723be4a6cbc604da6e2c1cac5
1RO8s <saki0220hiro@gmail.com>
2024-06-17 02:06:15 +0900

    monitorスレッド修正途中

--- philo/srcs/main.c
+++ philo/srcs/main.c
@@ -6,7 +6,7 @@
 /*   By: hnagasak <hnagasak@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/27 23:21:22 by hnagasak          #+#    #+#             */
-/*   Updated: 2024/06/16 04:25:17 by hnagasak         ###   ########.fr       */
+/*   Updated: 2024/06/16 06:54:58 by hnagasak         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -138,12 +138,12 @@ void	waiting_for_forks(t_philo *philo, pthread_mutex_t *fork1,
 		pthread_mutex_t *fork2)
 {
 	if (pthread_mutex_lock(fork1) != 0)
-		printf("Error: Failed to lock mutex\n");
+		printf("Error: Failed to lock fork1[%p]\n", fork1);
 	mutex_print(philo, TAKE_FORK);
 	if (pthread_mutex_lock(fork2) != 0)
 	{
 		pthread_mutex_unlock(fork1);
-		printf("Error: Failed to lock mutex\n");
+		printf("Error: Failed to lock fork2[%p]\n", fork2);
 	}
 	mutex_print(philo, TAKE_FORK);
 }
@@ -157,17 +157,16 @@ int	eat(t_philo *philo)
 	{
 		pthread_mutex_unlock(philo->right_fork);
 		pthread_mutex_unlock(philo->left_fork);
-		mutex_print(philo, TAKE_FORK);
 		return (1);
 	}
 	mutex_print(philo, EATING);
-	// mutex_print(philo, TEST);
 	usleep(philo->config->time_to_eat * 1000);
 	// printf("%ld\t%d [test]\n", us2ms(get_elapsed_usec(philo->config->start)),
 	// philo->id);
 	// mutex_print(philo, TEST);
 	pthread_mutex_unlock(philo->right_fork);
 	pthread_mutex_unlock(philo->left_fork);
+	mutex_print(philo, TEST);
 	// 食事後に死んでいないか確認
 	if (should_stop(philo))
 		return (1);
@@ -193,7 +192,7 @@ void	*handle_philo_actions(void *args)
 		// 食事
 		if (eat(philo))
 			break ;
-
+		break ;
 		// config->eat_count[philo->id]++;
 		// config->last_eat_time[philo->id] = get_elapsed_usec(us2timeval(config->last_eat_time[philo->id]));
 		// 睡眠
@@ -239,7 +238,7 @@ pthread_mutex_t	**init_forks(size_t num)
 	pthread_mutex_t	**forks;
 	size_t			i;
 
-	forks = (pthread_mutex_t **)malloc(sizeof(pthread_mutex_t *) * (num + 1));
+	forks = (pthread_mutex_t **)malloc(sizeof(pthread_mutex_t *) * num);
 	if (forks == NULL)
 	{
 		printf("Error: Failed to allocate memory for forks array\n");
@@ -250,28 +249,26 @@ pthread_mutex_t	**init_forks(size_t num)
 		forks[i] = (pthread_mutex_t *)malloc(sizeof(pthread_mutex_t));
 		if (forks[i] == NULL)
 		{
-			// Free memories if it fails to allocate memory
 			printf("Error: Failed to allocate memory for mutex\n");
 			while (i > 0)
 				free(forks[--i]);
 			free(forks);
 			return (NULL);
 		}
+		printf("# forks[%zu]: %p\n", i, forks[i]);
 		if (pthread_mutex_init(forks[i], NULL) != 0)
 		{
-			// Free memories if it fails to initialize mutex
 			printf("Error: Failed to initialize mutex\n");
 			while (i > 0)
 			{
-				pthread_mutex_destroy(forks--[i]);
+				pthread_mutex_destroy(forks[--i]);
 				free(forks[i]);
 			}
+			free(forks[i]); // The last allocated mutex also needs to be freed
 			free(forks);
 			return (NULL);
 		}
 	}
-	free(forks[num]);
-	forks[num] = NULL;
 	return (forks);
 }
 
@@ -302,7 +299,8 @@ int	is_invalid_argument(int argc, char **argv)
 
 void	wait_for_threads(pthread_t *pthreads, size_t num_of_philo)
 {
-	size_t		i;
+	size_t	i;
+
 	i = 0;
 	while (i < num_of_philo)
 	{
@@ -323,9 +321,23 @@ void	start_threads(pthread_t *pthreads, t_philo *data, size_t num_of_philo)
 		if (pthread_create(&pthreads[i], NULL, &handle_philo_actions,
 				&data[i]) != 0)
 			mutex_message(data[0].config, "Failed to create thread\n");
-			// perror("Failed to create thread");
+		// perror("Failed to create thread");
 		i++;
 	}
+}
+
+void	print_config_forks(t_config *config)
+{
+	size_t	i;
+
+	i = 0;
+	printf("--- print_config_forks ---\n");
+	while (i < config->num_of_philo)
+	{
+		printf("config->forks[%zu]: %p\n", i, (void *)config->forks[i]);
+		i++;
+	}
+	printf("\n");
 }
 
 void	init_philos(t_philo *philos, t_config *config)
@@ -335,12 +347,39 @@ void	init_philos(t_philo *philos, t_config *config)
 	i = 0;
 	while (i < config->num_of_philo)
 	{
+		printf("config->forks @philos[%zu] 0\n", i);
+		print_config_forks(config);
+		// philos[i].left_fork = NULL;
+		// philos[i].right_fork = NULL;
 		philos[i].id = i;
 		philos[i].left_fork = config->forks[i];
-		philos[i].right_fork = config->forks[(i + 1) % config->num_of_philo];
+		if (i + 1 != config->num_of_philo)
+		{
+			printf("config->forks @philos[%zu] 01\n", i);
+			print_config_forks(config);
+			philos[i].right_fork = config->forks[i + 1];
+			printf("config->forks @philos[%zu] 02\n", i);
+			print_config_forks(config);
+		}
+		else
+		{
+			philos[i].right_fork = config->forks[0];
+		}
+
+		printf("config->forks @philos[%zu] 03\n", i);
+		print_config_forks(config);
 		philos[i].eat_count = 0;
+		printf("config->forks @philos[%zu] 04\n", i);
+		print_config_forks(config);
+
+		printf("config->forks @philos[%zu] 1\n", i);
+		print_config_forks(config);
 		philos[i].last_eat_timeval = config->start;
+		printf("config->forks @philos[%zu] 2\n", i);
+		print_config_forks(config);
 		philos[i].config = config;
+		printf("config->forks @philos[%zu] 3\n", i);
+		print_config_forks(config);
 		i++;
 	}
 }
@@ -360,7 +399,6 @@ pthread_mutex_t	**free_forks(pthread_mutex_t **forks)
 	return (NULL);
 }
 
-
 // void *monitor(void *args)
 // {
 // 	(void) args;
@@ -369,55 +407,57 @@ pthread_mutex_t	**free_forks(pthread_mutex_t **forks)
 // 	return (NULL);
 // }
 
-
-
 int	main(int argc, char **argv)
 {
-	pthread_t		*pthreads;
-	pthread_t		th_monitor;
-	t_config		conf;
-	t_philo			*philos;
+	pthread_t *pthreads;
+	// pthread_t th_monitor;
+	t_config conf;
+	t_philo *philos;
 	// t_philo			*philo_args;
-	pthread_mutex_t	**forks;
+	pthread_mutex_t **forks;
 
 	// pthread_t		*pthreads;
 	if (is_invalid_argument(argc, argv))
 		return (EXIT_FAILURE);
 
-	
 	// initialize
 	philos = (t_philo *)malloc(sizeof(t_philo) * conf.num_of_philo);
-	
+
 	init_config(argc, argv, &conf);
 	forks = init_forks(conf.num_of_philo);
+	printf("forks[2]: %p\n", forks[2]);
+	printf("forks[3]: %p\n", forks[3]);
 	conf.forks = forks;
+	printf("conf.forks[2]: %p\n", conf.forks[2]);
+	printf("conf.forks[3]: %p\n", conf.forks[3]);
 	init_philos(philos, &conf);
 	conf.philos = philos;
 	print_philos_forks(philos, conf.num_of_philo);
-	
+
 	// start threads
 	pthreads = (pthread_t *)malloc(sizeof(pthread_t) * conf.num_of_philo);
 
 	start_threads(pthreads, philos, conf.num_of_philo);
-	if (pthread_create(&th_monitor, NULL, &monitor, &conf) != 0)
-		mutex_message(&conf, "Failed to create monitor thread\n");
-		// perror("Failed to create monitor thread");
-	else 
-		mutex_message(&conf, "### SUCCESS create monitor thread!!\n");
-	
+	// pthread_create(&th_monitor, NULL, &monitor, NULL);
+	// if (pthread_create(&th_monitor, NULL, &monitor, &conf) != 0)
+	// 	mutex_message(&conf, "Failed to create monitor thread\n");
+	// else
+	// 	mutex_message(&conf, "### SUCCESS create monitor thread!!\n");
+	// if (pthread_create(&th_monitor, NULL, &monitor, &conf) != 0)
+	// 	perror("Failed to create monitor thread\n");
+	// else
+	// 	perror("### SUCCESS create monitor thread!!\n");
 
 	// wait for threads
 	mutex_message(&conf, "### waiting for threads!!!!\n");
-	wait_for_threads(pthreads, conf.num_of_philo);
-	if (pthread_join(th_monitor, NULL) != 0)
-		perror("Failed to join monitor thread");
+	// wait_for_threads(pthreads, conf.num_of_philo);
+	// if (pthread_join(th_monitor, NULL) != 0)
+	// 	perror("Failed to join monitor thread");
 
-		
 	// free
 	free(pthreads);
 	free(philos);
 	forks = free_forks(forks);
 	printf("complete\n");
 	return (0);
-
 }
--- philo/srcs/monitor.c
+++ philo/srcs/monitor.c
@@ -6,7 +6,7 @@
 /*   By: hnagasak <hnagasak@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/13 04:18:17 by hnagasak          #+#    #+#             */
-/*   Updated: 2024/06/16 03:02:36 by hnagasak         ###   ########.fr       */
+/*   Updated: 2024/06/16 07:06:17 by hnagasak         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,47 +22,48 @@ typedef enum e_monitor_result
 
 void *monitor(void *args)
 {
-	int *result;
+	// int *result;
 	t_config *config;
-	size_t i;
-	size_t num_of_philo;
-	size_t fullfilled_philos;
-	t_philo *philos;
+	// size_t i;
+	// size_t num_of_philo;
+	// size_t fullfilled_philos;
+	// t_philo *philos;
 
-	result = (int *)malloc(sizeof(int));
+	// result = (int *)malloc(sizeof(int));
+	
 
 	
 	config = (t_config *)args;
-	philos = config->philos;
+	// philos = config->philos;
 	mutex_message(config,"--- begin monitor thread!! ---\n");
-	num_of_philo = config->num_of_philo;
-	while (1)
-	{
-		// 各哲学者の状態を監視する
-		fullfilled_philos = 0;
-		i = 0;
-		while (i < num_of_philo)
-		{
-			return (NULL);
-			// i番目の哲学者が死んでいたらDIEDを返す
-			if (philo_is_dead(&philos[i]))
-			{
-				config->is_anyone_dead = 1;
-				mutex_print(&philos[i], DEAD);
-				*result = DIED;
-				return (result);
-			}
-			// i番目の哲学者が必要な回数だけ食事を終えていたらfullfilled_philosをインクリメント
-			if (philos[i].eat_count >= config->required_eat_count)
-				fullfilled_philos++;
-			i++;
-		}
-		if (fullfilled_philos == num_of_philo)
-		{
-			*result = FULLFILLED;
-			return (result);
+	// num_of_philo = config->num_of_philo;
+	// while (1)
+	// {
+	// 	// 各哲学者の状態を監視する
+	// 	fullfilled_philos = 0;
+	// 	i = 0;
+	// 	while (i < num_of_philo)
+	// 	{
+	// 		return (NULL);
+	// 		// i番目の哲学者が死んでいたらDIEDを返す
+	// 		if (philo_is_dead(&philos[i]))
+	// 		{
+	// 			config->is_anyone_dead = 1;
+	// 			mutex_print(&philos[i], DEAD);
+	// 			*result = DIED;
+	// 			return (result);
+	// 		}
+	// 		// i番目の哲学者が必要な回数だけ食事を終えていたらfullfilled_philosをインクリメント
+	// 		if (philos[i].eat_count >= config->required_eat_count)
+	// 			fullfilled_philos++;
+	// 		i++;
+	// 	}
+	// 	if (fullfilled_philos == num_of_philo)
+	// 	{
+	// 		*result = FULLFILLED;
+	// 		return (result);
 
-		}
-	}
+	// 	}
+	// }
 	return (NULL);
 }
