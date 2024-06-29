c9b265dab3327a6ffad90b48a8beb1ffc8180899
1RO8s <saki0220hiro@gmail.com>
2024-06-30 01:17:36 +0900

    出力する番号の修正

--- philo/srcs/print.c
+++ philo/srcs/print.c
@@ -6,7 +6,7 @@
 /*   By: hnagasak <hnagasak@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/27 19:53:53 by hnagasak          #+#    #+#             */
-/*   Updated: 2024/06/27 19:59:16 by hnagasak         ###   ########.fr       */
+/*   Updated: 2024/06/30 01:15:03 by hnagasak         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,21 +23,23 @@ void	mutex_message(t_config *config, char *message)
 void	mutex_print(t_philo *philo, t_status status)
 {
 	long	elapsed_msec;
+	int		philo_num;
 
+	philo_num = philo->id + 1;
 	pthread_mutex_lock(&philo->config->print_mutex);
 	elapsed_msec = us2ms(get_elapsed_usec(philo->config->start));
 	if (status == DEAD)
-		printf("%ld\t%d died\n", elapsed_msec, philo->id);
+		printf("%ld\t%d died\n", elapsed_msec, philo_num);
 	else if (status == EATING)
-		printf("%ld\t%d is eating\n", elapsed_msec, philo->id);
+		printf("%ld\t%d is eating\n", elapsed_msec, philo_num);
 	else if (status == SLEEPING)
-		printf("%ld\t%d is sleeping\n", elapsed_msec, philo->id);
+		printf("%ld\t%d is sleeping\n", elapsed_msec, philo_num);
 	else if (status == THINKING)
-		printf("%ld\t%d is thinking\n", elapsed_msec, philo->id);
+		printf("%ld\t%d is thinking\n", elapsed_msec, philo_num);
 	else if (status == TAKE_FORK)
-		printf("%ld\t%d has taken a fork\n", elapsed_msec, philo->id);
+		printf("%ld\t%d has taken a fork\n", elapsed_msec, philo_num);
 	else if (status == TEST)
-		printf("%ld\t%d test\n", elapsed_msec, philo->id);
+		printf("%ld\t%d test\n", elapsed_msec, philo_num);
 	pthread_mutex_unlock(&philo->config->print_mutex);
 }
 
